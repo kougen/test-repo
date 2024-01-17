35a2057bf52af26a2d330c8493b9d9c2e7ad50aa
Nathan Hegedus <hegedusnathan5@gmail.com>
2024-01-17 20:32:00 +0100

    hello2

--- hello.cpp
+++ hello.cpp
@@ -5,8 +5,7 @@ using namespace std;
 int main()
 {
     cout << "Hello World" << endl;
-    cout << "Hello World1" << endl;
-    cout << "Hello World2" << endl;
+    cout << "This line is by Nathan" << endl;
     return 0;
 }
 
